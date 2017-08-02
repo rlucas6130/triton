a68b89603fb4ff565267fb7b97a194d7a4e51f03
rlucas6130 <ry.j.lucas@gmail.com>
2017-08-01 21:50:25 -0600

    Final create job methods.

--- UI/src/app/createJob.component.ts
+++ UI/src/app/createJob.component.ts
@@ -31,21 +31,25 @@ export class CreateJobComponent implements OnInit, DoCheck {
 
     ngDoCheck(...args: any[]): void {
 
-        this.uploader.queue = _.uniqBy(this.uploader.queue, 'file.name');
+        if (this.uploader.queue.length > 0 && !this.uploader.isUploading) {
 
-        for (let file of this.uploader.queue)
-        {
-            if (!_.some(this.documents, { name: file.file.name })) {
-                this.documents.unshift({
-                    id: 0,
-                    name: file.file.name,
-                    isSelected: false,
-                    isUploading: false
-                } as Document);
+            this.uploader.queue = _.uniqBy(this.uploader.queue, 'file.name')
+
+            this.uploader.queue = _.filter(this.uploader.queue, (fi) => {
+                return !_.some(_.filter(this.documents, (d) => d.id > 0), { name: fi.file.name });
+            });
+
+            for (let file of this.uploader.queue) {
+                if (!_.some(this.documents, { name: file.file.name })) {
+                    this.documents.unshift({
+                        id: 0,
+                        name: file.file.name,
+                        isSelected: false,
+                        isUploading: false
+                    } as Document);
+                }
             }
         }
-
-        this.documents = _.uniqBy(this.documents, 'name');
     }
 
     public selectAllDocuments(): void {
@@ -66,6 +70,54 @@ export class CreateJobComponent implements OnInit, DoCheck {
                 doc.isUploading = true;
             }
         }
+
+        this.waitUntilComplete();
+    }
+
+    public uploadDocument(document: Document): void {
+        _.find(this.uploader.queue, (fI) => fI.file.name == document.name).upload();
+
+        document.isUploading = true;
+
+        this.waitUntilComplete();
+    }
+
+    private waitUntilComplete(): void {
+        var intervalId = setInterval(() => {
+
+            var uploadingDocs = _.filter(this.documents, (doc) => {
+                return doc.id == 0 && doc.isUploading == true;
+            });
+
+            if (uploadingDocs.length == 0) {
+                clearInterval(intervalId);
+            }
+
+            this.documentService.getDocuments()
+                .then(docs => {
+                    for (let uploadingDoc of uploadingDocs) {
+                        var savedDoc = _.find(docs, (doc) => doc.name == uploadingDoc.name);
+
+                        if (savedDoc) {
+                            var docsIndex = _.findIndex(this.documents, (doc) => doc.name == uploadingDoc.name);
+                            this.documents[docsIndex].id = savedDoc.id;
+                            this.documents[docsIndex].isUploading = false;
+                        }
+                    }
+                });
+        }, 1000);
+    }
+
+    public removeDocument(document: Document): void {
+        _.find(this.uploader.queue, (fI) => fI.file.name == document.name).remove();
+
+        _.remove(this.documents, document);
+    }
+
+    public removeAllUploads(): void {
+        this.uploader.clearQueue();
+
+        _.remove(this.documents, (doc) => doc.id == 0);
     }
 
     public docIdTracker(index: number, doc: Document): string {
